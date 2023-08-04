906e956a78832e9a031a1ec20793b58b6fecd95d
palmcivet7 <palmcivet@proton.me>
2023-08-05 00:24:30 +0100

    add unchecked to loops and reuse msg.sender instead of new variable

--- GasOptimisationFoundry/src/Gas.sol
+++ GasOptimisationFoundry/src/Gas.sol
@@ -76,13 +76,12 @@ contract GasContract {
     History[] public paymentHistory; // when a payment was updated
 
     modifier onlyAdminOrOwner() {
-        address senderOfTx = msg.sender;
-        if (checkForAdmin(senderOfTx)) {
-            if (!checkForAdmin(senderOfTx)) {
+        if (checkForAdmin(msg.sender)) {
+            if (!checkForAdmin(msg.sender)) {
                 revert NotAdmin();
             }
             _;
-        } else if (senderOfTx == contractOwner) {
+        } else if (msg.sender == contractOwner) {
             _;
         } else {
             revert NotAdmin();
@@ -90,11 +89,10 @@ contract GasContract {
     }
 
     modifier checkIfWhiteListed(address sender) {
-        address senderOfTx = msg.sender;
-        if (senderOfTx != sender) {
+        if (msg.sender != sender) {
             revert NotSender();
         }
-        uint256 usersTier = whitelist[senderOfTx];
+        uint256 usersTier = whitelist[msg.sender];
         if (usersTier <= 0) {
             revert NotWhitelisted();
         }
@@ -108,18 +106,17 @@ contract GasContract {
         contractOwner = msg.sender;
         totalSupply = _totalSupply;
 
-        for (uint256 i = 0; i < administrators.length; ++i) {
-            if (_admins[i] != address(0)) {
-                administrators[i] = _admins[i];
-                if (_admins[i] == contractOwner) {
-                    balances[contractOwner] = totalSupply;
-                } else {
-                    balances[_admins[i]] = 0;
-                }
-                if (_admins[i] == contractOwner) {
-                    emit supplyChanged(_admins[i], totalSupply);
-                } else if (_admins[i] != contractOwner) {
-                    emit supplyChanged(_admins[i], 0);
+        unchecked {
+            for (uint256 i = 0; i < administrators.length; ++i) {
+                if (_admins[i] != address(0)) {
+                    administrators[i] = _admins[i];
+                    if (_admins[i] == contractOwner) {
+                        balances[contractOwner] = totalSupply;
+                        emit supplyChanged(_admins[i], totalSupply);
+                    } else {
+                        balances[_admins[i]] = 0;
+                        emit supplyChanged(_admins[i], 0);
+                    }
                 }
             }
         }
@@ -131,9 +128,11 @@ contract GasContract {
 
     function checkForAdmin(address _user) public view returns (bool admin_) {
         bool admin = false;
-        for (uint256 i = 0; i < administrators.length; ++i) {
-            if (administrators[i] == _user) {
-                admin = true;
+        unchecked {
+            for (uint256 i = 0; i < administrators.length; ++i) {
+                if (administrators[i] == _user) {
+                    admin = true;
+                }
             }
         }
         return admin;
@@ -144,7 +143,7 @@ contract GasContract {
         return balance;
     }
 
-    function getTradingMode() public view returns (bool mode_) {
+    function getTradingMode() public pure returns (bool mode_) {
         bool mode = false;
         if (TRADE_FLAG == 1 || DIVIDEND_FLAG == 1) {
             mode = true;
@@ -155,16 +154,11 @@ contract GasContract {
     }
 
     function addHistory(address _updateAddress, bool _tradeMode) public returns (bool status_, bool tradeMode_) {
-        History memory history;
-        history.blockNumber = block.number;
-        history.lastUpdate = block.timestamp;
-        history.updatedBy = _updateAddress;
+        History memory history =
+            History({blockNumber: block.number, lastUpdate: block.timestamp, updatedBy: _updateAddress});
+
         paymentHistory.push(history);
-        bool[] memory status = new bool[](tradePercent);
-        for (uint256 i = 0; i < tradePercent; ++i) {
-            status[i] = true;
-        }
-        return ((status[0] == true), _tradeMode);
+        return (true, _tradeMode);
     }
 
     function getPayments(address _user) public view returns (Payment[] memory payments_) {
@@ -174,31 +168,33 @@ contract GasContract {
         return payments[_user];
     }
 
-    function transfer(address _recipient, uint256 _amount, string calldata _name) public returns (bool status_) {
-        address senderOfTx = msg.sender;
-        if (balances[senderOfTx] < _amount) {
+    function transfer(address _recipient, uint256 _amount, string calldata _name) public returns (bool) {
+        if (balances[msg.sender] < _amount) {
             revert InsufficientBalance();
         }
         if (bytes(_name).length >= 9) {
             revert NameTooLong();
         }
-        balances[senderOfTx] -= _amount;
-        balances[_recipient] += _amount;
-        emit Transfer(_recipient, _amount);
-        Payment memory payment;
-        payment.admin = address(0);
-        payment.adminUpdated = false;
-        payment.paymentType = PaymentType.BasicPayment;
-        payment.recipient = _recipient;
-        payment.amount = _amount;
-        payment.recipientName = _name;
-        payment.paymentID = ++paymentCounter;
-        payments[senderOfTx].push(payment);
-        bool[] memory status = new bool[](tradePercent);
-        for (uint256 i = 0; i < tradePercent; ++i) {
-            status[i] = true;
+        unchecked {
+            balances[msg.sender] -= _amount;
+            balances[_recipient] += _amount;
         }
-        return (status[0] == true);
+
+        emit Transfer(_recipient, _amount);
+
+        Payment memory payment = Payment({
+            admin: address(0),
+            adminUpdated: false,
+            paymentType: PaymentType.BasicPayment,
+            recipient: _recipient,
+            amount: _amount,
+            recipientName: _name,
+            paymentID: ++paymentCounter
+        });
+
+        payments[msg.sender].push(payment);
+
+        return true;
     }
 
     function updatePayment(address _user, uint256 _ID, uint256 _amount, PaymentType _type) public onlyAdminOrOwner {
@@ -212,17 +208,17 @@ contract GasContract {
             revert InvalidAddress();
         }
 
-        address senderOfTx = msg.sender;
-
-        for (uint256 i = 0; i < payments[_user].length; ++i) {
-            if (payments[_user][i].paymentID == _ID) {
-                payments[_user][i].adminUpdated = true;
-                payments[_user][i].admin = _user;
-                payments[_user][i].paymentType = _type;
-                payments[_user][i].amount = _amount;
-                bool tradingMode = getTradingMode();
-                addHistory(_user, tradingMode);
-                emit PaymentUpdated(senderOfTx, _ID, _amount, payments[_user][i].recipientName);
+        unchecked {
+            for (uint256 i = 0; i < payments[_user].length; ++i) {
+                if (payments[_user][i].paymentID == _ID) {
+                    payments[_user][i].adminUpdated = true;
+                    payments[_user][i].admin = _user;
+                    payments[_user][i].paymentType = _type;
+                    payments[_user][i].amount = _amount;
+                    bool tradingMode = getTradingMode();
+                    addHistory(_user, tradingMode);
+                    emit PaymentUpdated(msg.sender, _ID, _amount, payments[_user][i].recipientName);
+                }
             }
         }
     }
@@ -231,45 +227,34 @@ contract GasContract {
         if (_tier >= 255) {
             revert InvalidTier();
         }
-        whitelist[_userAddrs] = _tier;
+
         if (_tier > 3) {
-            whitelist[_userAddrs] -= _tier;
             whitelist[_userAddrs] = 3;
-        } else if (_tier == 1) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 1;
-        } else if (_tier > 0 && _tier < 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 2;
-        }
-        uint256 wasLastAddedOdd = wasLastOdd;
-        if (wasLastAddedOdd == 1) {
-            wasLastOdd = 0;
-            isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
-        } else if (wasLastAddedOdd == 0) {
-            wasLastOdd = 1;
-            isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
+        } else if (_tier >= 1 && _tier <= 3) {
+            whitelist[_userAddrs] = _tier;
         } else {
-            revert ImpossibleError();
+            whitelist[_userAddrs] = 0;
         }
+
+        wasLastOdd = 1 - wasLastOdd;
+        isOddWhitelistUser[_userAddrs] = wasLastOdd;
+
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
     function whiteTransfer(address _recipient, uint256 _amount) public checkIfWhiteListed(msg.sender) {
-        address senderOfTx = msg.sender;
-        whiteListStruct[senderOfTx] = ImportantStruct(_amount, 0, 0, 0, true, msg.sender);
-
-        if (balances[senderOfTx] < _amount) {
-            revert InsufficientBalance();
-        }
         if (_amount <= 3) {
             revert InvalidAmount();
         }
-        balances[senderOfTx] -= _amount;
-        balances[_recipient] += _amount;
-        balances[senderOfTx] += whitelist[senderOfTx];
-        balances[_recipient] -= whitelist[senderOfTx];
-
+        if (balances[msg.sender] < _amount) {
+            revert InsufficientBalance();
+        }
+        uint256 senderWhitelistAmount = whitelist[msg.sender];
+        uint256 senderFinalBalance = balances[msg.sender] - _amount + senderWhitelistAmount;
+        uint256 recipientFinalBalance = balances[_recipient] + _amount - senderWhitelistAmount;
+        balances[msg.sender] = senderFinalBalance;
+        balances[_recipient] = recipientFinalBalance;
+        whiteListStruct[msg.sender] = ImportantStruct(_amount, 0, 0, 0, true, msg.sender);
         emit WhiteListTransfer(_recipient);
     }
 
